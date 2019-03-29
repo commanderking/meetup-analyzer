6d99f00f15b9613b514dd9b3fbd196d0219a499e
commanderking <jek582@mail.harvard.edu>
2019-03-28 23:20:49 -0400

    setup basic page

---
+++ src/features/singleMeetingAnalysis/SingleMeetingAnaylsisContainer.tsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+const SingleMeetingAnalysisContainer = () => {
+  return (
+    <div>
+      <h1>Meeting Statistics</h1>
+      <textarea />
+      <button>See Results</button>
+    </div>
+  );
+};
+
+export default SingleMeetingAnalysisContainer;
---
+++ src/features/test.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const test = () => {
+  return <div>Hey</div>;
+};
+
+export default test;
