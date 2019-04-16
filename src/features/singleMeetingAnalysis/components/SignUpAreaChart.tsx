356f61d40decf79db6304fa7cc7c77fafa118010
commanderking <jek582@mail.harvard.edu>
2019-04-15 21:00:15 -0400

    add tooltip with day of the week added

--- src/features/singleMeetingAnalysis/components/SignUpAreaChart.tsx
+++ src/features/singleMeetingAnalysis/components/SignUpAreaChart.tsx
@@ -1,81 +1,54 @@
 import React from "react";
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip } from "recharts";
+import { css, jsx } from "@emotion/core";
 
-import {
-  BarChart,
-  Bar,
-  Area,
-  XAxis,
-  YAxis,
-  CartesianGrid,
-  Tooltip
-} from "recharts";
-
-// const data = [
-//   {
-//     name: "Page A",
-//     uv: 4000,
-//     pv: 2400,
-//     amt: 2400
-//   },
-//   {
-//     name: "Page B",
-//     uv: 3000,
-//     pv: 1398,
-//     amt: 2210
-//   },
-//   {
-//     name: "Page C",
-//     uv: 2000,
-//     pv: 9800,
-//     amt: 2290
-//   },
-//   {
-//     name: "Page D",
-//     uv: 2780,
-//     pv: 3908,
-//     amt: 2000
-//   },
-//   {
-//     name: "Page E",
-//     uv: 1890,
-//     pv: 4800,
-//     amt: 2181
-//   },
-//   {
-//     name: "Page F",
-//     uv: 2390,
-//     pv: 3800,
-//     amt: 2500
-//   },
-//   {
-//     name: "Page G",
-//     uv: 3490,
-//     pv: 4300,
-//     amt: 2100
-//   }
-// ];
+const CustomTooltip = ({ active, payload }: any) => {
+  if (active) {
+    console.log(payload, "payload");
+    const { dayOfWeek, displayDate, count } = payload[0].payload;
+    return (
+      <div
+        id="tooltip"
+        css={css`
+          background-color: red;
+          text-align: left;
+          padding: 10px;
+        `}
+        style={{ backgroundColor: "white", padding: "10px", textAlign: "left" }}
+      >
+        <div>
+          {dayOfWeek}, {displayDate}
+        </div>
+        <div>Signups: {count}</div>
+      </div>
+    );
+  }
+  return null;
+};
 
 const SignUpAreaChart = ({ data }: any) => {
   console.log("react", React);
   console.log("data", data);
   return (
-    <BarChart
-      width={500}
-      height={400}
-      data={data}
-      margin={{
-        top: 10,
-        right: 30,
-        left: 0,
-        bottom: 0
-      }}
-    >
-      <CartesianGrid strokeDasharray="3 3" />
-      <XAxis dataKey="displayDate" />
-      <YAxis />
-      <Tooltip />
-      <Bar dataKey="count" fill="#8884d8" />
-    </BarChart>
+    <div>
+      <BarChart
+        width={500}
+        height={400}
+        data={data}
+        margin={{
+          top: 10,
+          right: 30,
+          left: 0,
+          bottom: 0
+        }}
+      >
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="displayDate" />
+        <YAxis />
+        <Tooltip content={<CustomTooltip />} />
+        <Bar dataKey="count" fill="#8884d8" />
+      </BarChart>
+    </div>
   );
 };
 
